328954c405b129bf62c22329c2a34d69b9c136a5
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2024-11-01 21:58:40 +0530

    deployment fixes

--- src/components/UserMgmt.tsx
+++ src/components/UserMgmt.tsx
@@ -183,16 +183,10 @@ const UserMgmt: React.FC = () => {
                       My Account
                     </p>
                     <hr />
-                    <button
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    >
+                    <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                       Profile
                     </button>
-                    <button
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    >
+                    <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                       Settings
                     </button>
                     <hr />
