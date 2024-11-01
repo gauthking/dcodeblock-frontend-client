e68a56fea5170e395071b431873e0a77d948ddec
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2024-11-01 21:12:37 +0530

    fixed env

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+require('dotenv').config()
 const instance = axios.create({
-    baseURL: "http://localhost:8001",
+    baseURL: process.env.baseURL,
 })
 
 export default instance
